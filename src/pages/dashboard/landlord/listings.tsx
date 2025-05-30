3d33c4721d3fde09a4ff0ec3799b6d40e1fe4977
Emmanuel Eguavoen <eguavoenemmanuel2019.com>
2025-05-30 15:03:01 +0100

    feat: designed the lanlord layout page

--- src/pages/dashboard/landlord/listings.tsx
+++ src/pages/dashboard/landlord/listings.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Listings() {
-  return <div>Listings</div>;
+  return (
+    <div className="main">
+      <div className="div1 first">1</div>
+      <div className="div1 sub1">2</div>
+      <div className="div2 sub2">3</div>
+    </div>
+  );
 }
