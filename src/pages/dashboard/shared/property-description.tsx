e9a3421342e653037bbfa86303c93165d9f650cd
ObodoakorDavid <obodoakordavid@gmail.com>
2025-09-10 14:46:31 +0100

    fixx

--- src/pages/dashboard/shared/property-description.tsx
+++ src/pages/dashboard/shared/property-description.tsx
@@ -23,7 +23,7 @@ export default function PropertyDescription() {
         <div className="flex gap-8">
           <div className="flex flex-col items-center">
             <Bed size={32} />
-            <p>{property?.numberOfBedRooms} bed</p>
+            <p>{property?.numberOfBedrooms} bed</p>
           </div>
           <div className="flex flex-col items-center">
             <Bath size={32} />
