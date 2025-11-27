9d822e28573ed29a667da55bbdea79b9474bf2f0
Emmanuel Eguavoen <eguavoenemmanuel2019.com>
2025-11-27 09:46:56 +0100

    chore: favourite API functionality

--- src/api/favourite.api.ts
+++ src/api/favourite.api.ts
@@ -28,52 +28,6 @@ class FavouriteService {
       handleAxiosError(error, "Unable to remove favourite");
     }
   }
-
-  // Get if property is favourited by logged-in user
-  async isFavourited(propertyId: string) {
-    try {
-      const response = await axiosInstance.get(
-        `/favourites/check/${propertyId}`
-      );
-      console.log("is favourited api", response.data);
-      return response.data?.favourited;
-    } catch (error) {
-      handleAxiosError(error, "Unable to check favourite status");
-    }
-  }
-
-  // Get logged-in user's favourites
-  async getUserFavourites() {
-    try {
-      const response = await axiosInstance.get(`/save-properties/user`);
-      console.log("user favourites api", response.data?.data);
-      return response.data?.data;
-    } catch (error) {
-      handleAxiosError(error, "Unable to get your favourites");
-    }
-  }
-
-  // Get favourite by ID
-  async getFavouriteById(favouriteId: string) {
-    try {
-      const response = await axiosInstance.get(`/favourites/${favouriteId}`);
-      console.log("favourite by id api", response.data?.data);
-      return response.data?.data;
-    } catch (error) {
-      handleAxiosError(error, "Unable to get favourite details");
-    }
-  }
-
-  // Get all favourites (admin)
-  async getAllFavourites() {
-    try {
-      const response = await axiosInstance.get(`/favourites`);
-      console.log("all favourites api", response.data?.data);
-      return response.data?.data;
-    } catch (error) {
-      handleAxiosError(error, "Unable to fetch favourites");
-    }
-  }
 }
 
 export const favouriteService = new FavouriteService();
